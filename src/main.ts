4767d5b843a39c9f2db5f054a324d2a7036e7a14
gajirou <lifelongsre@gmail.com>
2023-09-03 13:59:12 +0900

    Deno バージョンアップ

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,16 @@ import { yukukuru } from "./yukukuru.ts";
 import { Nostr, Relay } from "https://deno.land/x/nostr_deno_client/mod.ts";
 
 const relayList = {
-    // "lol" : "wss://nos.lol",
+    //"lol" : "wss://nos.lol",
     "Damus"   : "wss://relay.damus.io",
+    "Yabume"  : "wss//yabu.me",
     "Nokotaro": "wss://nostr-relay.nokotaro.com",
     "Relay-jp": "wss://relay-jp.nostr.wirednet.jp",
     "Relay.jp": "wss://relay.nostr.wirednet.jp"
 };
 
-const privateKey = Deno.env.get("PRIVATE_KEY");
 const nostr      = new Nostr();
-nostr.privateKey = privateKey;
+nostr.privateKey = Deno.env.get("PRIVATE_KEY");
 
 for(const key in relayList) {
   const value = relayList[key];
@@ -21,11 +21,12 @@ for(const key in relayList) {
   });
 }
 
-nostr.on('relayConnected', (relay: Relay) => console.log('Relay connected.', relay.name));
-nostr.on('relayError', (err: Error) => console.log('Relay error;', err));
-nostr.on('relayNotice', (notice: string[]) => console.log('Notice', notice));
+nostr.on("relayConnected", (relay: Relay) => console.log("Relay connected.", relay.name));
+nostr.on("relayError", (err: Error) => Deno.exit(console.log(err)));
+nostr.on("relayNotice", (notice: string[]) => console.log("Notice", notice));
 
 nostr.debugMode = true;
+
 await nostr.connect();
 await nostr.sendTextPost(yukukuru(true));
 await nostr.disconnect();
